25475b9992c9d9460fa9f7808ec50c23df680e19
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-07 14:34:11 +0100

    [FIX] fix the appearance condition of <origin_delivery_ref> and <origin_delivery_date> & fix translation.

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -7,5 +7,5 @@ from odoo.exceptions import UserError
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    origin_delivery_ref = fields.Char(string='Origin delivery reference')
-    origin_delivery_date = fields.Date(string='Origin delivery date')
+    origin_delivery_ref = fields.Char(string='Origin delivery reference',states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},)
+    origin_delivery_date = fields.Date(string='Origin delivery date',states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},)
